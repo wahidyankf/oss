d68c740eb9d3346bb36d493481b09947f2f196e0
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2025-02-09 18:01:54 +0700

    chore(ayokoding-web): Update playwright config

--- apps-standalone/ayokoding-web/playwright.config.ts
+++ apps-standalone/ayokoding-web/playwright.config.ts
@@ -11,11 +11,11 @@ export default defineConfig({
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
   },
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'npm run dev',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
   projects: [
     {
       name: 'chromium',
